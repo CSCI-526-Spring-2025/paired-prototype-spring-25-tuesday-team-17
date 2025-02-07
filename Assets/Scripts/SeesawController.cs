733674b01ca937618bbe24f5d601b88bdb9ad693
XZhou <a827414472@gmail.com>
2025-02-06 21:03:35 -0800

    Add a seesaw to the basic level

---
+++ Assets/Scripts/SeesawController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeesawController : MonoBehaviour
+{
+    [Header("Physics Settings")]
+    public float maxAngle = 45f;     // maximum rotation angle
+    public float balanceDamping = 2f; // rotation damping
+
+    private Rigidbody2D beamRb;
+    private HingeJoint2D hinge;
+
+    void Start()
+    {
+        beamRb = GetComponent<Rigidbody2D>();
+        hinge = GetComponent<HingeJoint2D>();
+    }
+
+    void FixedUpdate()
+    {
+        // limit the rotation angle
+        float currentAngle = hinge.jointAngle;
+        if (Mathf.Abs(currentAngle) > maxAngle)
+        {   
+            // Stop the beam from rotating further
+            beamRb.angularVelocity = 0f;
+            // beamRb.rotation = Mathf.Sign(currentAngle) * maxAngle;
+        }
+    }
+}
