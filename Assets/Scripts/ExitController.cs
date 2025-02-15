79f587ebc1fb33773c2f5f9e454290d4b3f4aa2d
XZhou <a827414472@gmail.com>
2025-02-14 19:24:07 -0800

    Update exit and add Wining Scene

---
+++ Assets/Scripts/ExitController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        GameObject collectiblePrefab = GameObject.Find("collectibles");
+        int totalCollectibles = 0;
+        totalCollectibles += collectiblePrefab.transform.childCount;
+        if (totalCollectibles == 0)
+        {
+            Debug.Log("All collectibles collected!");
+            if(other.CompareTag("Player"))
+            {
+                Debug.Log("Player has reached the exit!");
+                SceneManagement.LoadScene("WinScene");
+            }
+        }
+    }
+}
